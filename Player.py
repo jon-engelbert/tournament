78eb0a148f1d71a644bd6c518e0baa56f7ed327c
Jon Engelbert <jon.a.engelbert@gmail.com>
2015-02-24 13:35:02 -0500

    rename models

--- Player.py
+++ Player.py
@@ -1,28 +1,24 @@
-import datetime
-
 __author__ = 'jonengelbert'
 import tournament
-import datetime
 
-class Tourney:
-    def __init__(self, name, date=datetime.date.today(), location = ""):
+class Player:
+    def __init__(self, name, initcount = 0):
         self.name = name
-        self.date = date,
-        self.location = location
+        self.initcount = initcount
         self.id = None
 
     def delete_from_db(self):
-        """Remove all the tournament records from the database."""
+        """Remove all the player records from the database."""
         print("delete_from_id, name, id: %s %d" % (self.name, self.id))
         conn = tournament.connect()
         cursor = conn.cursor()
-        statement = "DELETE FROM tourney WHERE id = %d" % self.id
+        statement = "DELETE FROM player WHERE id = %d" % self.id
         cursor.execute(statement)
         conn.commit()
         conn.close()
 
     def add_to_db(self):
-        """Adds a tournament to the tournament database.
+        """Adds a player to the tournament database.
         The database assigns a unique serial id number for the player.  (This
         should be handled by your SQL database schema, not in your Python code.)
         Args:
@@ -30,8 +26,8 @@ class Tourney:
         """
         conn = tournament.connect()
         cursor = conn.cursor()
-        statement = "INSERT INTO  tourney (name, tourney_date, location) VALUES (%s, %s, %s) RETURNING ID"
-        data = (self.name, self.date, self.location)
+        statement = "INSERT INTO  player (name) VALUES (%s) RETURNING ID"
+        data = (self.name,)
         cursor.execute(statement, data)
         conn.commit()
         self.id = cursor.fetchone()[0]
--- Tourney.py
+++ Tourney.py
@@ -1,24 +1,26 @@
 __author__ = 'jonengelbert'
 import tournament
+import datetime
 
-class Player:
-    def __init__(self, name, initcount = 0):
+class Tourney:
+    def __init__(self, name, date=datetime.date.today(), location = ""):
         self.name = name
-        self.initcount = initcount
+        self.date = date,
+        self.location = location
         self.id = None
 
     def delete_from_db(self):
-        """Remove all the player records from the database."""
+        """Remove all the tournament records from the database."""
         print("delete_from_id, name, id: %s %d" % (self.name, self.id))
         conn = tournament.connect()
         cursor = conn.cursor()
-        statement = "DELETE FROM player WHERE id = %d" % self.id
+        statement = "DELETE FROM tourney WHERE id = %d" % self.id
         cursor.execute(statement)
         conn.commit()
         conn.close()
 
     def add_to_db(self):
-        """Adds a player to the tournament database.
+        """Adds a tournament to the tournament database.
         The database assigns a unique serial id number for the player.  (This
         should be handled by your SQL database schema, not in your Python code.)
         Args:
@@ -26,14 +28,10 @@ class Player:
         """
         conn = tournament.connect()
         cursor = conn.cursor()
-        statement = "INSERT INTO  player (name) VALUES (%s) RETURNING ID"
-        data = (self.name,)
+        statement = "INSERT INTO  tourney (name, tourney_date, location) VALUES (%s, %s, %s) RETURNING ID"
+        data = (self.name, self.date, self.location)
         cursor.execute(statement, data)
         conn.commit()
         self.id = cursor.fetchone()[0]
         conn.close()
         return
-
-
-
-
